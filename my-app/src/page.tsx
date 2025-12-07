88a7eba342b99b3d6fcfb87e82357a58f7965828
Builder User <builder@example.com>
2025-12-07 12:42:34 +0545

    Update

--- my-app/src/page.tsx
+++ my-app/src/page.tsx
@@ -1,1 +1,9 @@
-export default function Home() {   return (     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">       <h1>Hello world</h1>     </div>   ); }
+export default function Home() 
+{   
+  return (     
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">       
+      <h1>Hello world</h1>
+      <h2> make changes in the page.tsx in the src<h2>
+    </div>   
+  ); 
+}
